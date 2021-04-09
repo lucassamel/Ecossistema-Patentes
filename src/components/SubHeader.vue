5894216861a822487cbb0fb669c7bf86a6be2a4a
Lucas Samel <lucas.samel97@gmail.com>
2021-04-09 01:26:02 -0300

    headers

--- src/components/SubHeader.vue
+++ src/components/SubHeader.vue
@@ -1,51 +1,35 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark"  style="background-color: #3033D9">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+    <b-navbar
+      toggleable="lg"
+      type="dark"
+      style="background-color: #3033D9"
+      class="mx-auto"
+    >
+      <b-navbar-nav class="mx-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-button pill variant="primary" size="lg" class="mr-3"
+            >Vídeo Institucional</b-button
+          >
+          <b-button pill variant="primary" size="lg"
+            >Tenho interesse em saber mais</b-button
+          >
         </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
-
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
+.botao{
+    padding-right: 20px;
+}
 </style>
